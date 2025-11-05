4f95d1a929eb8d19a509f1cf9480faa23805e20b
JackTheFine <112029550+JackTheFine@users.noreply.github.com>
2025-11-04 21:36:24 -0500

    Create isabully.js

---
+++ bots/SKN/commands/isabully.js
@@ -0,0 +1,19 @@
+const { EmbedBuilder } = require('discord.js')
+const { ActionRowBuilder, ButtonBuilder, SlashCommandBuilder } = require('@discordjs/builders');
+const { ButtonStyle } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('isabully')
+    .setDescription('hello')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User')
+        .setRequired(true)
+    ),
+
+  async execute(interaction, client) {
+    const user = interaction.options.getUser('user');
+    interaction.channel.send(`${user} is a bully chain :point_down:`)
+  }
+}
